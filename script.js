73982a24650c46fb3ee8289dd78d1cf1e8513fde
Gerry Bukit <112598706+ifs21055-itdel@users.noreply.github.com>
2023-10-23 20:58:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,176 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+  let letters = word.textContent.split("");
+  word.textContent = "";
+  letters.forEach((letter) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.className = "letter";
+    word.append(span);
+  });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () => {
+  let currentWord = words[currentWordIndex];
+  let nextWord =
+    currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+  Array.from(currentWord.children).forEach((letter, i) => {
+    setTimeout(() => {
+      letter.className = "letter out";
+    }, i * 80);
+  });
+  nextWord.style.opacity = "1";
+  Array.from(nextWord.children).forEach((letter, i) => {
+    letter.className = "letter behind";
+    setTimeout(() => {
+      letter.className = "letter in";
+    }, 340 + i * 80);
+  });
+  currentWordIndex =
+    currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText, 3000);
+
+// sticky navbar ////////////////////////////////////////////////////////////////
+
+const header = document.querySelector("header"); // Corrected selector
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 50); // Corrected method
+});
+
+// circle skill ////////////////////////////////////////////////////////////////
+
+const circles = document.querySelectorAll(".circle");
+circles.forEach((elem) => {
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent");
+  var percent = Math.floor((dots * marked) / 100);
+  var points = "";
+  var rotate = 360 / dots;
+
+  for (let i = 0; i < dots; i++) {
+    points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+  }
+  elem.innerHTML = points;
+
+  const pointsMarked = elem.querySelectorAll(".points");
+  for (let i = 0; i < percent; i++) {
+    pointsMarked[i].classList.add("marked");
+  }
+});
+
+// mix it up portfolio section
+var mixer = mixitup(".portfolio-gallery", {
+  selectors: {
+    target: ".port-box",
+  },
+  animation: {
+    duration: 400,
+  },
+});
+
+// Event listener untuk mengaktifkan filter ketika tombol filter diklik
+var filterButtons = document.querySelectorAll(".btn");
+filterButtons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    var filterValue = this.getAttribute("data-filter");
+    mixer.filter(filterValue);
+  });
+});
+
+// active menu ////////////////////////////////////////////////////////////////
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+  menuLi.forEach((sec) => sec.classList.remove("active"));
+  menuLi[len].classList.add("active");
+}
+
+activeMenu(); // Corrected function call
+window.addEventListener("scroll", activeMenu);
+
+//Form validation
+const form = document.querySelector("form[name='contact-form']");
+const nameInput = document.querySelector("input[name='name']");
+const emailInput = document.querySelector("input[name='email']");
+const phoneInput = document.querySelector("input[name='phone']");
+const messageInput = document.querySelector("textarea[name='message']");
+
+nameInput.isValid = () => !!nameInput.value;
+emailInput.isValid = () => isValidEmail(emailInput.value);
+phoneInput.isValid = () => isValidPhone(phoneInput.value);
+messageInput.isValid = () => !!messageInput.value;
+
+const inputFields = [nameInput, emailInput, phoneInput, messageInput];
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const isValidPhone = (phone) => {
+  const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(String(phone).toLowerCase());
+};
+
+let shouldValidate = false;
+let isFormValid = false;
+
+const validateInputs = () => {
+  console.log("we are here");
+  if (!shouldValidate) return;
+
+  isFormValid = true;
+  inputFields.forEach((input) => {
+    input.classList.remove("invalid");
+    input.nextElementSibling.classList.add("hide");
+
+    if (!input.isValid()) {
+      input.classList.add("invalid");
+      isFormValid = false;
+      input.nextElementSibling.classList.remove("hide");
+    }
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  shouldValidate = true;
+  validateInputs();
+  if (isFormValid) {
+    // TODO: DO AJAX REQUEST
+  }
+});
+
+inputFields.forEach((input) => input.addEventListener("input", validateInputs));
+
+//popup contact
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  shouldValidate = true;
+  validateInputs();
+  if (isFormValid) {
+    // TODO: Lakukan AJAX REQUEST
+
+    // Tampilkan popup
+    const popup = document.getElementById("popup");
+    popup.style.display = "flex";
+
+    // Tambahkan event listener untuk tombol close
+    const closePopup = document.getElementById("close-popup");
+    closePopup.addEventListener("click", () => {
+      popup.style.display = "none";
+    });
+  }
+});
